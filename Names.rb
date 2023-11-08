bb35a19beb6382ece989158dac89702692df9a13
Nicholas Gibson <codebyzade@gmail.com>
2023-11-08 14:02:20 -0500

    fixed to output the correct string

--- Names.rb
+++ Names.rb
@@ -5,4 +5,4 @@ d = {first_name: "James", last_name: "Smith"}
 e = {first_name: "Jennifer", last_name: "Smith"}
 names = [a, b, c, d, e]
 
-names.each { |i| puts i[:first_name]}
+names.each { |i| puts "The name is '#{i[:first_name]} #{i[:last_name]}'"}
