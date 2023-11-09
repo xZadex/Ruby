4a023c6d9d16d8879b671010caecd9ff7eb46d2e
Nicholas Gibson <codebyzade@gmail.com>
2023-11-09 15:54:11 -0500

    updated notes

--- OOP.rb
+++ OOP.rb
@@ -105,20 +105,110 @@
 
 # In the last line of our script, we tried to chain methods. However, we get a NoMethodError that 'breath' is an undefined method for a nil value. The reason is that since the puts method prints the value and returns nil, the value that is getting returned from our instance methods is nil. To be able to chain methods, our instance methods need to return an object, more specifically, they need to return self.
 
-class Mammal
-    def initialize
-        puts "I am alive"
-        self
-    end
-    def breath  
-        puts "Inhale and exhale"
-        self
-    end
+# class Mammal
+#     def initialize
+#         puts "I am alive"
+#         self
+#     end
+#     def breath  
+#         puts "Inhale and exhale"
+#         self
+#     end
 
-    def who_am_i
-        puts self
-        self
-    end
-end
+#     def who_am_i
+#         puts self
+#         self
+#     end
+# end
 
-my_mammal = Mammal.new.who_am_i.breath
+# my_mammal = Mammal.new.who_am_i.breath
+
+
+# Visibility & Inheritance
+
+# Public
+# class Mammal
+# def breath  
+#     puts "Inhale and exhale"
+# end
+
+# def eat
+#     puts "Yum yum yum"
+# end
+# end
+# class Human < Mammal # Human inherits from Mammal
+# def subclass_method
+#     breath
+# end
+# def another_method
+#     self.eat
+# end
+# end
+# person = Human.new
+# person.subclass_method
+# person.another_methodCopy
+
+
+# Protected
+# class Mammal
+# # previous code removed for brevity
+
+# def calling_speak
+#     speak
+# end
+
+# protected
+#     def speak
+#     puts "I am a protected method"
+#     end
+# end
+# class Human < Mammal
+# # previous code removed for brevity
+
+# def explicitly_speak
+#     self.speak
+# end
+
+# def implicitily_speak
+#     speak
+# end
+# end
+# mammal = Mammal.new
+# mammal.speak # => protected method `speak' called for #<Mammal:0x007fa5438183d8> (NoMethodError)
+# mammal.calling_speak # => I am a protected method
+# person = Human.new
+# person.speak # => protected method `speak' called for #<Human:0x007fedfe824710> (NoMethodError)
+# person.explicitly_speak # => I am a protected method
+# person.implicitily_speak # => I am a protected methodCopy
+
+# Private
+# class Mammal
+# # previous code removed for brevity
+
+# def calling_cry
+#     cry
+# end
+
+# private
+#     def cry
+#     puts "Sniff sniff..."
+#     end
+# end
+# class Human < Mammal
+# # previous code removed for brevity
+
+# def explicitly_cry
+#     self.cry
+# end
+
+# def implicitly_cry
+#     cry
+# end
+# end
+# mammal = Mammal.new
+# mammal.calling_cry # => Sniff sniff...
+# mammal.cry # => private method `cry' called for #<Mammal:0x007fd9830de5f8> (NoMethodError)
+# person = Human.new
+# person.cry # => private method `cry' called for #<Human:0x007f8f298de248> (NoMethodError)
+# person.explicitly_cry # => `explicitly_cry': private method `cry' called for #<Human:0x007f87a30bf450> (NoMethodError)
+# person.implicitly_cry # => Sniff sniff...Copy
