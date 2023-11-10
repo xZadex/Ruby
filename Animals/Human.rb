50a09729269bdd803bce37d8c43671613c1a4b4e
Nicholas Gibson <codebyzade@gmail.com>
2023-11-10 10:29:11 -0500

    Requiring Files Examples

---
+++ Animals/Human.rb
@@ -0,0 +1,14 @@
+require_relative 'Mammal'
+
+class Human < Mammal
+    def subclass_method
+        breath
+    end
+
+    def another_method
+        self.eat
+    end
+end
+
+person = Human.new
+person.another_method
---
+++ Animals/Mammal.rb
@@ -0,0 +1,9 @@
+class Mammal
+    def breath  
+        puts "Inhale and exhale"
+    end
+
+    def eat
+        puts "Yum yum yum"
+    end
+end
